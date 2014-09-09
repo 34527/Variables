cd5dc3b8c54d0657dbaa8692ae89be55c1a2b82d
34527 <34527@longroad.ac.uk>
2014-09-09 12:55:46 +0100

    Name input Program

---
+++ Name_Input.py
@@ -0,0 +1,8 @@
+#Euan McElhoney
+#9/9/14
+#Basic Input Program
+
+print("Good day")                                       #displays quoted test.
+user_name = input("What is your name? ")                #user_name acts as a variable and input will take the users input and assign it to the variable.
+print(user_name)                                        #displays user_name variable. Quotes not needed as it is displaying a variable.
+print("Hello {0}.".format(user_name))                   #This is using a place holder to display the variable embedded within quoted text.
